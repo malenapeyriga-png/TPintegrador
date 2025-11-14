b67d78e07388f60928e1d9128e09fe8196e53721
malenapeyriga-png <malenapeyriga@gmail.com>
2025-11-14 03:06:30 -0300

    .H declaracion de funciones, variables y vectores
    PUNTO 2: PORCENTAJE DE VENTAS POR CADA MEDIO DE PAGO.
    
    PUNTO 3: VENTAS POR MARCA Y FP
    
    4.PRODUCTOS SIN VENTAS
    
    5_TOP 10 CLIENTES SORTEO DE CUPONES:

--- .github/workflows/.h
+++ .github/workflows/.h
@@ -1,48 +1,37 @@
-//carga de lote y porcentajeVentasFP
-//invoco funciones
+DECLARACION DE FUNCIONES, VARIABLES, VECTORES .H:
+//lote carga de formas de pago:
+ char codFP[5];
+    string nomFP[5];
+    float porcentajes[5];
+    int i;
 
-float porcentajeCantVendidaFP(codFP[5],Cant[],porcentajesCantVendida[5])
- //FUNCION CALCULA PORCENTAJE.(con desarrollo).
-        float porcentajeCantVendidaFP(int num1, int num2):
-            pce=(num1*100)/num2;
-            return pce;
-//ventas por marca y fp
-    float acuEF[]={};
+//PUNTO 2: PORCENTAJE DE VENTAS POR CADA MEDIO DE PAGO.
+ float porcentajeCantVendidaFP(codFP[5],Cant[],porcentajesCantVendida[5]):
+ acuCant=0;
+ FP=1;
+//FUNCION CALCULA PORCENTAJE.
+   float porcentaje(int num1, int num2):
+
+//PUNTO 3: VENTAS POR MARCA Y FP
+
+float acuEF[]={};
     float acuMP[]={};
     float acuTR[]={};
     float acuTC[]={};
     float acuCT[]={};
     int i;
-    while (nroCompra=!0):{
-        for(i=0;i<10,i++):{
-            if codProd==codProd[i];
-                codProd[i];
-                codMarca[i];
-                indice=i;
-                switch():
-                    cout<<"Seleccione un medio de pago";
-                    cout<<"1.Efectivo, 2.Mercado Pago, 3.transferencia, 4.tarjeta de credito, 5.criptomonedas";
-                case 1:
-                    //efectivo
-                    acuEF[INDICE]+=Cant;
-                case 2:
-                    //Mercado pago
-                    acuMP[INDICE]+=Cant;
-                case 3:
-                    //transferencia
-                    acuTR[INDICE]+=Cant;
-                case 1:
-                    //Tarjeta credito
-                    acuTC[INDICE]+=Cant;
-                case 1:
-                    //Criptomonedas
-                    acuCT[INDICE]+=Cant;
-            for(i=0;i<10,i++):{
-                cout<<"Total de ventas efectivo:",acuEF[i],"Codigo de marca",codMarca[i];
-                cout<<"Total de ventas Mercado Pago:",acuMP[i],"Codigo de marca",codMarca[i];
-                cout<<"Total de ventas transferencia:",acuTR[i],"Codigo de marca",codMarca[i];
-                cout<<"Total de ventas tarjeta de crèdito:",acuTC[i],"Codigo de marca",codMarca[i];
-                cout<<"Total de ventas criptomoneda:",acuCT[i],"Codigo de marca",codMarca[i];
-            }
-        }
-    }
+
+//4.PRODUCTOS SIN VENTAS
+void productoSinVentas(bool Vprod[20],int NroCompra,)
+
+5_TOP 10 CLIENTES SORTEO DE CUPONES:
+
+int codCliente[50]  
+int MAX[10]: {};   
+
+//funcion ordenar vector de menor a mayor
+void ordenarVector(int v[], int tam )
+//cargarAleatorio
+	void cargarAleatorio(int v[], int tam, int limite)
+//mostrarVector
+ void mostrarVector(int v[], int tam)
